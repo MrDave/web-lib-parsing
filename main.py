e3f20c229e802d2222d9a3f8cd2f53567ec070f0
MrDave <davidschnoll@gmail.com>
2023-10-18 19:08:21 +0300

    Download books by ID from tululu.org

--- main.py
+++ main.py
@@ -3,12 +3,22 @@ from environs import Env
 from pathlib import Path
 
 
-def download_file(path, url, params=None):
+def download_book(path, book_id):
+    url = "https://tululu.org/txt.php"
+    params = {
+        "id": book_id
+    }
     response = requests.get(url, params=params)
     response.raise_for_status()
+    check_for_redirect(response)
 
     with open(path, "wb") as file:
         file.write(response.content)
+
+
+def check_for_redirect(response):
+    if response.history and response.url == "https://tululu.org/":
+        raise requests.HTTPError
 
 
 def main():
@@ -17,10 +27,13 @@ def main():
 
     media_folder = env.str("MEDIA_FOLDER")
     Path(media_folder).mkdir(exist_ok=True, parents=True)
-    book_link = "https://tululu.org/txt.php?id=32168"
-    path = Path(media_folder).joinpath("Пески Марса.txt")
 
-    download_file(path, book_link)
+    for book_id in range(1, 11):
+        path = Path(media_folder).joinpath(f"book_{book_id}.txt")
+        try:
+            download_book(path, book_id)
+        except requests.HTTPError:
+            continue
 
 
 if __name__ == '__main__':
