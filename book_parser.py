5f6969db4537aad1e215a3ced6a79f7805989be3
MrDave <davidschnoll@gmail.com>
2023-11-11 15:09:46 +0200

    Proof vs GET params errors

--- book_parser.py
+++ book_parser.py
@@ -26,18 +26,22 @@ def retry_on_failure(max_retries=5, exceptions=(Exception,)):
 
 
 @retry_on_failure(exceptions=(requests.ConnectionError, requests.Timeout))
-def download_txt(url, filename, media_folder):
+def download_txt(book_id, filename, media_folder):
     """Download text files.
 
     Args:
-        url (str): url of a file to download.
+        book_id (int): id of the book to download.
         filename (str): file name to save the file as (without ".txt" extension).
         media_folder (str): folder to save the file to.
 
     Returns:
         str: filepath of the saved file.
     """
-    response = requests.get(url)
+    url = "https://tululu.org/txt.php"
+    params = {
+        "id": book_id
+    }
+    response = requests.get(url, params=params)
     response.raise_for_status()
     check_for_redirect(response)
     if not is_valid_filename(filename):
@@ -64,14 +68,13 @@ def fetch_book_page(book_id: int):
     return response
 
 
-def parse_book_page(response, book_id: int):
-    """Parse book webpage for book info, comments and download/cover links."""
+def parse_book_page(response):
+    """Parse book webpage for book info, comments and cover link."""
 
     soup = BeautifulSoup(response.text, "lxml")
     title_tag = soup.find("body").find("h1")
     book_title = title_tag.text.split("::")[0].strip()
     book_author = title_tag.find("a").text
-    book_link = f"https://tululu.org/txt.php?id={book_id}"
 
     image_tag = soup.find(class_="bookimage").find("img")
     image_link = f"https://tululu.org{image_tag["src"]}"
@@ -82,7 +85,7 @@ def parse_book_page(response, book_id: int):
     genre_tags = soup.find("span", class_="d_book").find_all("a")
     genres = [tag.text for tag in genre_tags]
 
-    return book_title, book_author, book_link, image_link, comments, genres
+    return book_title, book_author, image_link, comments, genres
 
 
 @retry_on_failure(exceptions=(requests.ConnectionError, requests.Timeout))
@@ -130,9 +133,9 @@ def main():
     for book_id in range(args.start_id, args.end_id + 1):
         try:
             book_page = fetch_book_page(book_id)
-            title, author, book_link, image_link, comments, genres = parse_book_page(book_page, book_id)
+            title, author, image_link, comments, genres = parse_book_page(book_page)
             filename = f"{book_id}. {title}"
-            download_txt(book_link, filename, book_folder)
+            download_txt(book_id, filename, book_folder)
             download_image(image_link, image_folder)
             print(
                 f"Title: {title}\n"
